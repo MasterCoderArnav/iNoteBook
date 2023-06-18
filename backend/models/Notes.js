40743fe8e9ba69cfc62e5abf355b21afaecc3141
unknown <f20200082@pilani.bits-pilani.ac.in>
2023-06-18 10:44:57 +0530

    Added createNote and fetchAllNotes requests for backend

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const notesSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title: {
         type: String,
         required: true,
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const fetchUser = require("../middleware/fetchUser");
+const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-    res.json([]);
+// Route 1: Fetch all the notes using: GET "/api/notes/fetchAllNotes". Login required
+router.get("/fetchAllNotes", fetchUser, async (req, res) => {
+    try{
+        let notes = await Notes.find({ user: req.user.id });
+        res.json(notes);
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
+});
+
+// Route 2: Add a new note using: POST "/api/notes/addNote". Login required
+router.post("/addNote", fetchUser,[
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({min: 5}),
+    body("tag", "Tag must be atleast 3 characters").isLength({min: 3}),
+], async (req, res) => {
+    try{
+        const error = validationResult(req);
+        if(!error.isEmpty()){
+            return res.status(400).json({error: error.array()});
+        }
+        const { title, description, tag } = req.body;
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save();
+        res.json(savedNote);
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
 });
 
 module.exports = router;
